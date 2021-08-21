4e916df5a87d900fdf596ad90439c87bc5eafe25
ME <mebener44@gmail.com>
2021-08-21 16:05:28 -0700

    updated mongoose

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/book-search', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+
   useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 module.exports = mongoose.connection;
